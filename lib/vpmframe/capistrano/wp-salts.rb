5886df507ebacb46f6308a28019caf0d0edaec1f
Chris Van Patten <chris@vanpattenmedia.com>
2012-12-10 13:19:52 -0500

    Add new tools to generate/symlink wp salts

---
+++ lib/vpmframe/capistrano/wp-salts.rb
@@ -0,0 +1,22 @@
+unless Capistrano::Configuration.respond_to?(:instance)
+  abort "capistrano-vpmframe requires Capistrano 2+"
+end
+
+Capistrano::Configuration.instance.load do
+
+namespace :salts do
+  
+  desc "Generate new salts"
+  task :generate_wp_salts, :roles => :app do
+    run "mkdir -p #{shared_path}/config"
+    run "echo '<?php' > #{shared_path}/config/wp-salts.php && curl https://api.wordpress.org/secret-key/1.1/salt >> #{shared_path}/config/wp-salts.php"
+  end
+
+  desc "Symlink salts"
+  task :symlink_wp_salts, :roles => :app do
+    run "#{sudo} ln -nfs #{shared_path}/config/wp-salts.php #{release_path}/config/wp-salts.php"
+  end
+
+end
+
+end

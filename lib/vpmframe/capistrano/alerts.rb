b9eee2ea733c48a9226ce9efd4d2ecd2413d05a2
Chris Van Patten <chris@vanpattenmedia.com>
2014-05-19 09:21:24 -0400

    change alert style, suppress output on curl

--- lib/vpmframe/capistrano/alerts.rb
+++ lib/vpmframe/capistrano/alerts.rb
@@ -8,7 +8,7 @@ namespace :alerts do
 
   desc "Notify Hipchat"
   task :hipchat, :roles => :app do
-    system("curl -d 'room_id=#{fetch(:alerts_hipchat_room)}&from=DeployBot&message=(successful)+#{fetch(:application)}+deployed+successfully+to+#{fetch(:app_stage)}&color=green' https://api.hipchat.com/v1/rooms/message\?auth_token\=#{fetch(:alerts_hipchat_key)}\&format\=json")
+    system("curl -s -d 'room_id=#{fetch(:alerts_hipchat_room)}&from=DeployBot&message=\<strong\>#{fetch(:application)}\<\/strong\>+deployed+successfully+to+\<strong\>#{fetch(:app_stage)}\<\/strong\>+at+\<strong\>#{fetch(:app_server)}\<\/strong\>&color=green' https://api.hipchat.com/v1/rooms/message\?auth_token\=#{fetch(:alerts_hipchat_key)}\&format\=json")
   end
 
 end

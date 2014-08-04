7cac77046ef773ba7222c9dd5c9f1ebfc7601db7
Chris Van Patten <chris@vanpattenmedia.com>
2014-08-04 09:13:36 -0400

    0.8.0 - better tempdir handling

--- lib/vpmframe/capistrano/assets.rb
+++ lib/vpmframe/capistrano/assets.rb
@@ -12,25 +12,21 @@ namespace :assets do
 
   desc "Get local clone"
   task :local_temp_clone, :roles => :app do
-    # Clear out any existing local clones
-    system("rm -rf ~/.captemp")
-
     # Get the local clone
-    system("mkdir -p ~/.captemp")
-    system("git clone #{fetch(:repository)} ~/.captemp/#{fetch(:application)}")
+    system("git clone #{fetch(:repository)} #{fetch(:tmpdir)}/#{fetch(:application)}")
 
     # Update submodules
-    system("cd ~/.captemp/#{fetch(:application)} && git submodule init && git submodule update")
+    system("cd #{fetch(:tmpdir)}/#{fetch(:application)} && git submodule init && git submodule update")
   end
 
 
   ##  
-  # TODO: Better support for make stuff
+  # Compile assets
   ##
 
   desc "Compile local assets"
   task :compile_local_assets, :roles => :app do
-    system("cd ~/.captemp/#{fetch(:application)} && make assets")
+    system("cd #{fetch(:tmpdir)}/#{fetch(:application)} && make assets")
   end
 
 
@@ -38,35 +34,9 @@ namespace :assets do
   # Upload
   ##
   
-  desc "Upload all local assets (only works if files are deployed to top-level assets folder)"
+  desc "Upload all local assets"
   task :upload_local_assets, :roles => :app do
-    system("scp -r -P #{fetch(:app_port)} ~/.captemp/#{fetch(:application)}/public/assets #{fetch(:user)}@#{fetch(:app_server)}:#{release_path}/public/")
-  end
-
-  desc "Upload compiled CSS"
-  task :upload_asset_css, :roles => :app do
-    # Make remote dir
-    run "mkdir -p #{release_path}/public/content/themes/#{fetch(:app_theme)}/css/"
-
-    # Upload assets
-    system("scp -r -P #{fetch(:app_port)} ~/.captemp/#{fetch(:application)}/public/content/themes/#{fetch(:app_theme)}/css #{fetch(:user)}@#{fetch(:app_server)}:#{release_path}/public/content/themes/#{fetch(:app_theme)}/")
-  end
-
-  desc "Upload compiled JS"
-  task :upload_asset_js, :roles => :app do
-    # Make remote dir
-    run "mkdir -p #{release_path}/public/content/themes/#{fetch(:app_theme)}/js/"
-
-    # Upload assets
-    system("scp -r -P #{fetch(:app_port)} ~/.captemp/#{fetch(:application)}/public/content/themes/#{fetch(:app_theme)}/js #{fetch(:user)}@#{fetch(:app_server)}:#{release_path}/public/content/themes/#{fetch(:app_theme)}/")
-  end
-
-  desc "Upload compiled images"
-  task :upload_asset_images, :roles => :app do
-    # Make remote dir
-    run "mkdir -p #{release_path}/public/content/themes/#{fetch(:app_theme)}/img/"
-
-    system("scp -r -P #{fetch(:app_port)} ~/.captemp/#{fetch(:application)}/public/content/themes/#{fetch(:app_theme)}/img #{fetch(:user)}@#{fetch(:app_server)}:#{release_path}/public/content/themes/#{fetch(:app_theme)}/")
+    system("scp -r -P #{fetch(:app_port)} #{fetch(:tmpdir)}/#{fetch(:application)}/public/assets #{fetch(:user)}@#{fetch(:app_server)}:#{release_path}/public/")
   end
 
 
@@ -76,7 +46,7 @@ namespace :assets do
 
   desc "Cleanup local copy"
   task :local_temp_cleanup, :roles => :app do
-    system("rm -rf ~/.captemp")
+    system("rm -rf #{fetch(:tmpdir)}")
   end
 
 end

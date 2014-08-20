b3f51b7b111c0ef886b85ef2b0259496276f6e8a
Chris Van Patten <chris@vanpattenmedia.com>
2014-08-20 09:04:44 -0400

    0.9.0

--- lib/vpmframe/capistrano/credentials.rb
+++ lib/vpmframe/capistrano/credentials.rb
@@ -6,24 +6,54 @@ Capistrano::Configuration.instance.load do
 
 namespace :credentials do
 
+  ##
+  # Database credentials
+  ##
+
   desc "Upload database credentials to the shared directory"
   task :upload_db_cred, :roles => :app do
     upload("./config/database.yml", "#{shared_path}/config/database.yml")
   end
 
-  desc "Upload S3 credentials to the shared directory"
-  task :upload_s3_cred, :roles => :app do
-    upload("./config/s3.yml", "#{shared_path}/config/s3.yml")
+  desc "Symlink database credentials to the current release directory"
+  task :symlink_db_cred, :roles => :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
 
-  desc "Upload S3 credentials to the shared directory"
+
+  ##
+  # htpasswd credentials
+  ##
+
+  desc "Upload htpasswd credentials to the shared directory"
   task :upload_htpasswd_cred, :roles => :app do
     upload("./config/puppet/templates/nginx/htpasswd", "#{shared_path}/config/htpasswd")
   end
 
-  desc "Symlink database credentials to the current release directory"
-  task :symlink_db_cred, :roles => :app do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+
+  ##
+  # Services folder
+  ##
+
+  desc "Upload credentials to the shared directory"
+  task :upload_services_cred, :roles => :app do
+    run "mkdir -p #{shared_path}/config"
+    upload("./config/services", "#{shared_path}/config", :via => :scp, :recursive => :true)
+  end
+
+  desc "Symlink services credentials to the current release directory"
+  task :symlink_services_cred, :roles => :app do
+    run "#{sudo} ln -nfs #{shared_path}/config/services #{release_path}/config/services"
+  end
+
+
+  ##
+  # Deprecated
+  ##
+
+  desc "Upload S3 credentials to the shared directory"
+  task :upload_s3_cred, :roles => :app do
+    upload("./config/s3.yml", "#{shared_path}/config/s3.yml")
   end
 
 end
--- vpmframe.gemspec
+++ vpmframe.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |gem|
   gem.name          = "vpmframe"
-  gem.version       = "0.8.0"
+  gem.version       = "0.9.0"
 
   gem.authors       = ["Chris Van Patten"]
   gem.email         = ["info@vanpattenmedia.com"]

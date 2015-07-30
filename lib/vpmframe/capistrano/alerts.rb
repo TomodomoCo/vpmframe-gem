24489644f896ca1f2100d329ac75b19bd6f00de5
Chris Van Patten <chris@vanpattenmedia.com>
2015-07-30 07:14:23 -0400

    add native support for slack alerts

--- lib/vpmframe/capistrano/alerts.rb
+++ lib/vpmframe/capistrano/alerts.rb
@@ -6,9 +6,14 @@ Capistrano::Configuration.instance.load do
 
 namespace :alerts do
 
-  desc "Notify Hipchat"
+  desc "Deploy alert for HipChat"
   task :hipchat, :roles => :app do
     system("curl -s -d 'room_id=#{fetch(:alerts_hipchat_room)}&from=DeployBot&message=\<strong\>#{fetch(:application)}\<\/strong\>+deployed+successfully+to+\<strong\>#{fetch(:app_stage)}\<\/strong\>+at+\<strong\>#{fetch(:app_server)}\<\/strong\>&color=green' https://api.hipchat.com/v1/rooms/message\?auth_token\=#{fetch(:alerts_hipchat_key)}\&format\=json >> /dev/null")
+  end
+
+  desc "Deploy alert on Slack"
+  task :slack, :roles => :app do
+    system("curl -s -X POST --data-urlencode 'payload={\"channel\": \"#{fetch(:alerts_slack_room)}\", \"username\": \"deploybot\", \"text\": \"*#{fetch(:application)}* deployed *#{fetch(:current_revision)}* successfully to *#{fetch(:app_stage)}* at *#{fetch(:app_server)}*\", \"icon_emoji\": \":computer:\"}' #{fetch(:alerts_slack_hook)} > /dev/null")
   end
 
 end
--- vpmframe.gemspec
+++ vpmframe.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |gem|
   gem.name          = "vpmframe"
-  gem.version       = "0.9.2"
+  gem.version       = "0.9.3"
 
   gem.authors       = ["Chris Van Patten"]
   gem.email         = ["info@vanpattenmedia.com"]
